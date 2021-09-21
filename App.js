47e08ac6ddcc6292cde3679c4dbe957e284d8e2d
Mohammed Faizan Mohiuddin <faizanmohiuddin482@gmail.com>
2021-09-21 22:15:18 +0530

    Added conditions and edit functionality

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React,{useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {
   SafeAreaView,
@@ -17,43 +17,112 @@ import {
   useColorScheme,
   View,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Button,
 } from 'react-native';
 
 import QRCode from 'react-native-qrcode-svg';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
-  const [inputText,setInputText] = useState('');
-  const [qrvalue,setQrvalue] = useState('');
-  console.log(inputText,'input')
+  const [inputText, setInputText] = useState('');
+  const [qrvalue, setQrvalue] = useState('');
+  const [asyncValue, setAsyncValue] = useState('');
+  const [valExist, setValExist] = useState(false);
+
+  const storeData = async value => {
+    try {
+      await AsyncStorage.setItem('number', value);
+      setAsyncValue(value);
+      setQrvalue(value);
+      setValExist(true);
+    } catch (e) {
+      console.log(e);
+      // saving error
+    }
+  };
+
+  useEffect(async () => {
+    try {
+      const value = await AsyncStorage.getItem('number');
+      if (value !== null) {
+        // value previously stored
+        setAsyncValue(value);
+        setQrvalue(value);
+        setValExist(true);
+      }
+      console.log(value, 'val');
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+  console.log(valExist, 'valExist');
+  const changeIt =()=>{
+    setValExist(false);
+  }
+
   return (
-    <SafeAreaView style={{flex:1}}>
+    <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
-     <QRCode value={qrvalue ? qrvalue : 'NA'} />
-     <TextInput text={inputText} onChangeText={(text)=>setInputText(text)} placeholder='Enter Phone Number' />
-     <TouchableOpacity style={styles.button} onPress={()=>setQrvalue(inputText)}>
-       <Text>Generate</Text>
-     </TouchableOpacity>
-     </View>
+        <QRCode size={350} value={qrvalue ? qrvalue : 'NA'} />
+        {valExist ? (
+          <View>
+            <Text style={{marginTop:20}}>{asyncValue}</Text>
+            <TouchableOpacity  style={styles.editButton} onPress={changeIt} >
+              <Text style={styles.buttonText}>Edit</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View>
+            <TextInput
+              style={styles.inputField}
+              text={inputText}
+              onChangeText={text => setInputText(text)}
+              placeholder={asyncValue ? asyncValue : 'Enter Phone Number'}
+            />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => storeData(inputText)}>
+              <Text style={styles.buttonText}>Generate</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
- container:{
-   flex:1,
-   alignItems:'center',
-   justifyContent:'center',
-   textAlign:'center',
-   backgroundColor:'white'
- },
- button:{
-   backgroundColor:'red',
-   padding:10,
-   alignItems:'center',
-   marginTop:30
- }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: 'white',
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  editButton: {
+    backgroundColor: 'black',
+    padding: 5,
+    alignItems: 'center',
+    marginTop: 5,
+    width:40,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  inputField: {
+    flexDirection: 'row',
+    marginTop: 30,
+    borderWidth: 2,
+    height: 40,
+    width: 200,
+  },
 });
 
 export default App;
